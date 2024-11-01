0320eb088025c9ffd152107421f227aafd8d2b51
Kairavparikh <kairav.parikh01@gmail.com>
2024-11-01 14:01:15 -0400

    ThirdCommit

--- CapStoneProject/src/Main.java
+++ CapStoneProject/src/Main.java
@@ -1,5 +1,6 @@
 public class Main implements Runnable {
     private int sum = 0;
+    static long total = 0;
     private int threadNum;
     public Main(int threadNum) {
         this.threadNum = threadNum;
@@ -13,6 +14,7 @@ public class Main implements Runnable {
             thread[i] = new Thread(obj);
             thread[i].start();
         }
+        System.out.println(total + " ");
     }
 
     @Override
@@ -20,6 +22,9 @@ public class Main implements Runnable {
         for (int j = 1; j <= 1000000; j++) {
             sum += j;
         }
+        total += sum;
         System.out.println("Running thread for i = " + threadNum + ", sum = " + sum);
+        System.out.println("The sum so far from thread " + threadNum + " is = " + total);
     }
+
 }
