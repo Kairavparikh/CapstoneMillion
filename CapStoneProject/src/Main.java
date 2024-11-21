df6feebadd67d7a2e63a92b3ef326c40fe7df1bf
Kairavparikh <kairav.parikh01@gmail.com>
2024-11-21 12:09:58 -0500

    singleThreadVersion

--- CapStoneProject/src/Main.java
+++ CapStoneProject/src/Main.java
@@ -1,21 +1,20 @@
 public class Main implements Runnable {
-    private int sum = 0;//used to count each threads sum
     static long total = 0;//used to count the total of all the threads
-    private int threadNum; //tracks which out of the 1000 thread we are on
+    private int threadNum;
+    private int count = 0;//tracks which out of the 1000 thread we are on
+
     public Main(int threadNum) {//constructor to initialize thread and sum
         this.threadNum = threadNum;
-        this.sum = 0;
     }
     public static void main(String[] args) {//main method
         long start = System.nanoTime(); // tracks initial time
         System.out.print("Hello and welcome!");
-        Thread[] thread = new Thread[1000]; // main thread
-        for(int i = 0; i < thread.length; i++){
-            Main obj = new Main(i); // creates main object for thread
-            thread[i] = new Thread(obj); // initializes the threads
-            thread[i].start(); // starts all the thread
-
-        }
+            Thread[] thread = new Thread[1]; // main thread
+            for (int i = 0; i < thread.length; i++) {
+                Main obj = new Main(i); // creates main object for thread
+                thread[i] = new Thread(obj); // initializes the threads
+                thread[i].start(); // starts all the thread
+            }
         for(int i = 0; i < thread.length; i++) {
             try {
                 thread[i].join(); // join them after they have finished executing the threads
@@ -26,16 +25,16 @@ public class Main implements Runnable {
         System.out.println("The final total is " + total);
         long end = System.nanoTime(); // end time
         long totalTime = end - start; // delta t
-        System.out.println("Thread execution time: " + totalTime / 1_000_000.0 + " ms");
+        System.out.println("Thread execution time: " + totalTime / 1000000.0 + " ms");
     }
 
     @Override
     public void run() {
         for (int j = 1; j <= 1000000; j++) {
-            sum += j; //sums the values from 1 - 1M
+            count++; //sums the values from 1 - 1M
         }
-        total += sum; // final total of all the threads
-        System.out.println("Running thread for i = " + threadNum + ", sum = " + sum);
+        total += count; // final total of all the threads
+        System.out.println("Running thread for i = " + threadNum + ", sum = " + count);
         System.out.println("The sum so far from thread " + threadNum + " is = " + total);
     }
 
